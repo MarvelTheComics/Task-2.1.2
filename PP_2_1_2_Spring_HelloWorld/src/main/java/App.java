2d6c5f7d2d243c0ff1bdce13a16db6e889209c9b
MarvelTheComics <46871573+MarvelTheComics@users.noreply.github.com>
2026-04-16 17:48:09 +0300

    task 2.1.2 - completed

--- PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -6,7 +6,17 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        HelloWorld newBean =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat newCat = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Проверка HelloWorld: " + (bean == newBean));
+        System.out.println("Проверка Cat: " + (cat == newCat));
+
     }
 }
